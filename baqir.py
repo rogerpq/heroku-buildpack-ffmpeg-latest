5d1b40e702ffde83770c567ffa890ebcc346e37e
روجر <104784913+rogerpq@users.noreply.github.com>
2023-11-10 16:23:42 +0300

    Create baqir.py

---
+++ baqir.py
@@ -0,0 +1,16 @@
+from baqir import zq_lo
+import repthon
+
+# English
+
+WARNING: Please If you want to take the file, 
+ask permission from the developer because the developer has made a build Operate successfully
+Thank you for your understanding.
+Contact the developer: https://telegram.dog/E_7_V
+
+#arabic
+
+تحذير: من فضلك إذا كنت تريد أن تأخذ الملف
+اطلب الإذن من المطور لأن المطور قد قام ببناء التشغيل بنجاح
+شكرا لتفهمك.
+اتصل بالمطور: https://telegram.dog/E_7_V
